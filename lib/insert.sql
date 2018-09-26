06405f439ce7010d688d8ff9159acc533004ece9
Melissa Real <melissa.real@hotmail.com>
2018-09-26 18:25:23 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Mr. Chocolate', 1, 'F', 'fudge', 'happy', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Rowdy', 3, 'F', 'blue', 'happy', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Tabitha', 10, 'M', 'black', 'happy', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Sergeant Brown', 9, 'M', 'fudge', 'happy', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Melissa', 8, 'F', 'rainbow', 'stuffed', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Grinch', 7, 'M', 'mint', 'happy', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Wendy', 4, 'F', 'fuschia', 'happy', 1);
+ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ (NULL, 3, 'M', 'yellow', 'angry', 1 );
